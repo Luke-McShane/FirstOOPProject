d97aa8553494ebad46829e3a1dc92d0370d5bc84
Luke McShane <lukejsmcshane@gmail.com>
2024-08-07 11:13:54 +0100

    Continued mapping out interfaces and classes. Created basic template.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,46 @@
-﻿public interface IVehicle
+﻿
+using System.Dynamic;
+
+public static class Program
+{
+  private static readonly Random getRandom = new Random();
+  private static readonly string newLine = Environment.NewLine;
+  private static readonly List<string> cities = new List<string> {"London", "Birmingham", "Edinburgh", "Sheffield",
+        "Nottingham", "Leeds", "Glasgow", "Cardiff", "Aberystwyth", "Manchester"};
+  private static readonly string city = cities[GetRandomnNumber(0, cities.Count - 1, getRandom)];
+  private static readonly int daysToComplete = GetRandomnNumber(6, 12, getRandom);
+  public static int Money { get; set; } = 12000;
+  public static void Run()
+  {
+    System.Console.WriteLine($"Welcome to X! On this journey, you will be travelling to {city}! You have {daysToComplete} to get there!");
+    System.Console.WriteLine(@$"You have £{Money} to spend on buying a car. You will also have the opportunity to make improvements/
+                                customisations to your car before you start your journey, so it may be better to save some money for this. Also, you
+                                might want to save some money for if your vehicle breaks down on the journey.");
+    System.Console.WriteLine(@"Each day will be different on your journey.The weather will change, and you will come across different challenges,
+                               forcing you to make a decision which may impact the stress of the driver, the condition of the car, and the amount
+                               of time taken to get to the destination. And, you must consider that a single mistake could lead you to not being
+                               able to complete the journey!");
+    System.Console.WriteLine(@$"If you make it to the finish line, you will receive a score based on:{newLine}--> The amount of days you had to spare.
+                                {newLine}--> The stress level of your driver.
+                                {newLine}--> How much cash you had to spare.
+                                {newLine}--> How 'cool' your car is (a paintjob will help in this regard).");
+  }
+
+  private static int GetRandomnNumber(int min, int max, Random getRandom)
+  {
+    lock (getRandom)
+    {
+      return getRandom.Next(min, max);
+    }
+  }
+}
+public interface IVehicle
 {
   public void Refuel(int amount);
   public void Drive(int distance);
 }
 
-public abstract class Vehicle(List<Wheel> wheels, Suspension suspension, Engine engine, FuelTank fuelTank, Colour colour, int topSpeed) : IVehicle
+public abstract class Vehicle(List<Wheel> wheels, Suspension suspension, Engine engine, FuelTank fuelTank, Colour colour, int topSpeed, int mpg, string name) : IVehicle
 {
   public List<Wheel> Wheels { get; set; } = wheels;
   public Suspension Suspension { get; set; } = suspension;
@@ -12,55 +48,51 @@ public abstract class Vehicle(List<Wheel> wheels, Suspension suspension, Engine 
   public FuelTank FuelTank { get; set; } = fuelTank;
   public Colour Colour { get; set; } = colour;
   public int TopSpeed { get; init; } = topSpeed;
-  protected int _milesUntilNoFuel;
-  protected int _milesPerGallon;
+  public double MilesUntilNoFuel { get; set; } = GetMilesUntilNoFuel(mpg, fuelTank.RemaningFuelInLitres);
+  public int MilesPerGallon { get; } = mpg;
+  public string Name { get; } = name
 
-  public Vehicle(List<Wheel> wheels, Suspension suspension, Engine engine, FuelTank fuelTank, Colour colour, int topSpeed)
+  public static double GetMilesUntilNoFuel(int mpg, int litresOfFuelLeft)
   {
-
+    return Math.Round(mpg * (litresOfFuelLeft / 4.54), 1);
   }
 
   public void Drive(int distance)
   {
-    _milesUntilNoFuel -= distance;
+    MilesUntilNoFuel -= distance;
   }
 
   public void Refuel(int amount)
   {
     throw new NotImplementedException();
   }
-
-  private void calculateMPG()
-  {
-    _milesPerGallon =
-  }
 }
 
-public abstract class FuelTank
+public class FuelTank
 {
   public int MaxCapacity { get; init; }
-  public float RemaningFuel { get; set; }
-  public string ShowRemaning => $"You have {this.RemaningFuel} miles left.";
+  public int RemaningFuelInLitres { get; set; }
+  public string ShowRemaning => $"You have {RemaningFuelInLitres} miles left.";
 }
 
-public abstract class Wheel
+public class Wheel
 {
   public bool IsPunctured { get; set; } = false;
   public TyreType Type { get; init; }
 }
 
-public abstract class Damageable
+public class Damageable
 {
   public bool IsDamaged { get; set; } = false;
 }
 
-public abstract class Suspension : Damageable
+public class Suspension : Damageable
 {
   public SuspensionType Type { get; init; }
 }
 
 
-public abstract class Engine : Damageable
+public class Engine : Damageable
 {
   public float Litres { get; set; }
 }
@@ -69,7 +101,6 @@ public class Driver : Damageable
 {
   public int Money;
   public int StressLevel;
-
 }
 
 public enum Colour
@@ -97,4 +128,59 @@ public enum SuspensionType
 {
   Hard,
   Soft
+}
+
+public enum Cities
+{
+  London,
+  Birmingham,
+  Edinburgh,
+  Sheffield,
+  Nottingham,
+  Leeds,
+  Glasgow,
+  Cardiff,
+  Aberystwyth,
+  Manchester
+};
+
+public abstract class SportsCar : Vehicle
+{
+  protected SportsCar(List<Wheel> wheels, Suspension suspension, Engine engine, FuelTank fuelTank, Colour colour, int topSpeed, int mpg, string name) : base(wheels, suspension, engine, fuelTank, colour, topSpeed, mpg, name)
+  {
+  }
+}
+
+public abstract class Motorbike : Vehicle
+{
+  protected Motorbike(List<Wheel> wheels, Suspension suspension, Engine engine, FuelTank fuelTank, Colour colour, int topSpeed, int mpg, string name) : base(wheels, suspension, engine, fuelTank, colour, topSpeed, mpg, name)
+  {
+  }
+}
+
+public abstract class SUV : Vehicle
+{
+  protected SUV(List<Wheel> wheels, Suspension suspension, Engine engine, FuelTank fuelTank, Colour colour, int topSpeed, int mpg, string name) : base(wheels, suspension, engine, fuelTank, colour, topSpeed, mpg, name)
+  {
+  }
+}
+
+public abstract class Hatchback : Vehicle
+{
+  protected Hatchback(List<Wheel> wheels, Suspension suspension, Engine engine, FuelTank fuelTank, Colour colour, int topSpeed, int mpg, string name) : base(wheels, suspension, engine, fuelTank, colour, topSpeed, mpg, name)
+  {
+  }
+}
+
+public enum Vehicles
+{
+  FordFocus,
+  FiatPanda,
+  RangeRover,
+  JeepWrangler,
+  KawasakiNinja,
+  KTMEnduro,
+  BMWM3,
+  LamborghiniGallardo
+
 }
